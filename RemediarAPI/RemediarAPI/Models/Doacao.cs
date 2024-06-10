71c9b3a5702ed991beb2566a0b62d0fb5d74c618
LucasFerreiraGuedes <lucasguedes01673874@gmail.com>
2024-06-09 21:50:38 -0300

    Retirando propriedades desnecessárias da entidade Doacao

--- RemediarAPI/RemediarAPI/Models/Doacao.cs
+++ RemediarAPI/RemediarAPI/Models/Doacao.cs
@@ -7,14 +7,7 @@
         public int qtdCaixas { get; set; }
         public int qtdMg { get; set; }
         public DateTime dtValidade { get; set; }
-        public string nomeDoador { get; set; }
-        public string telefone { get; set; }
-        public string endereco { get; set; }
         public DateTime dtRetirada { get; set; }
-        public string horario { get; set; }
-        public string status { get; set; }
-        public DateTime data { get; set; }
-        public DateTime dataRetirada { get; set; }
         public double valorDoacao { get; set; }
         public int usuarioId { get; set; }
         public Usuario? Usuario { get; set; }
