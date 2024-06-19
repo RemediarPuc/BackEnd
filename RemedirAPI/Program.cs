d793bd0abccc24abda505bd4a2c5a347161820c8
NicolasMatheusFerreira <nicolasmatheusferreira13@gmail.com>
2024-06-18 21:27:21 -0300

    Corrigindo classe db

--- RemedirAPI/Program.cs
+++ RemedirAPI/Program.cs
@@ -1,8 +1,23 @@
+using Microsoft.EntityFrameworkCore;
+using RemedirAPI.Context;
+using System.Text.Json.Serialization;
+using RemedirAPI.Context;
+
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+});
+
+
+IServiceCollection serviceCollection = builder.Services.AddDbContext<ContextDb>(c =>
+{
+    c.UseSqlServer(builder.Configuration.GetConnectionString("DbConnectionString"));
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -19,6 +34,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+app.UseCors(x => x.WithOrigins("http://localhost:4200").AllowAnyMethod().AllowAnyHeader());
+
 
 app.MapControllers();
 
